377d99eab0e84482108a24258265988c5402b263
Alex Sears <alexwsears@gmail.com>
2013-11-27 20:46:20 -0500

    Initial commit

---
+++ src/Searsaw/Drawbridge/DrawbridgeServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Searsaw\Drawbridge;
+
+use Illuminate\Support\ServiceProvider;
+
+class DrawbridgeServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('searsaw/drawbridge');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
